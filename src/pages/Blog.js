3d2ef0716e7d73099ff26c2c0132d957b3cde65b
Eivind Omtveit <e.omtveit@gmail.com>
2020-05-27 13:51:35 +0200

    added camera svg

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Camera from '../assets/Camera'
 
 class Blog extends Component {
     render() {
         return(
             <div className="blog-container">
-                var på H&M idag, fashion disæster lizm
+                <Camera></Camera>
             </div>
         )
     }
