12d9b96769714b13528f5aa7e64cf36118012e39
Eivind Omtveit <e.omtveit@gmail.com>
2020-05-27 19:21:12 +0200

    added images

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Camera from '../assets/Camera'
+import RenderP from '../assets/images/Render-persp-1.png'
+import RenderF from '../assets/images/Render-front-1.png'
+
+
+
 
 class Blog extends Component {
+
     render() {
         return(
             <div className="blog-container">
-                <Camera></Camera>
+                <img className="render" src={RenderP} alt="rendered camera"/>
+                <img className="render" src={RenderF} alt="rendered camera"/>
+                <Camera stroke="white"></Camera>
             </div>
         )
     }
